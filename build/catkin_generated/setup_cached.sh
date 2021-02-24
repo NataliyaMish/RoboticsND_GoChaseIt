1d2829266d16b76135a2a88f5035eda5290ec280
Nataliya Mishukova <nataliya.mishukova@gmail.com>
2021-02-24 21:22:11 +0000

    refactor: Add wheels

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,7 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/workspace/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/workspace/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
 export PATH="/opt/ros/kinetic/bin:/usr/bin:/root/miniconda3/bin:/opt/pytorch/bin:/opt/utils/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/VirtualGL/bin:/opt/TurboVNC/bin:/opt/swift/swift-4.0.3-RELEASE-ubuntu16.04/usr/bin"
+export PKG_CONFIG_PATH="/home/workspace/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/workspace/catkin_ws/build"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/catkin_ws/devel/share/common-lisp"
 export ROS_PACKAGE_PATH="/home/workspace/catkin_ws/src:$ROS_PACKAGE_PATH"
